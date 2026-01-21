dd1ab25192258575ac91d9cfea83b53c684aaa1c
babisRoutis <haralampos.routis@netcompany.com>
2026-01-21 15:36:39 +0200

    Expand `SignatureVerification` enum in `IsChainTrusted`: add detailed signature validation types for EU entities, certificates, and status lists.

--- consultation/src/commonMain/kotlin/eu/europa/ec/eudi/etsi119602/consultation/IsChainTrusted.kt
+++ consultation/src/commonMain/kotlin/eu/europa/ec/eudi/etsi119602/consultation/IsChainTrusted.kt
@@ -21,18 +21,82 @@ import eu.europa.ec.eudi.etsi119602.profile.*
 public fun interface IsChainTrusted<in CHAIN : Any> {
 
     public enum class SignatureVerification {
+        /**
+         * Check the wallet provider's signature for a WIA
+         * Can be used by an Authorization Server implementing
+         * Attestation-Based Client Authentication
+         */
         EU_WIA,
+
+        /**
+         * Check the wallet provider's signature for a WUA
+         * Can be used by a Credential Issuer, issuing device-bound attestations
+         * that require WUA
+         */
         EU_WUA,
+
+        /**
+         * Check the wallet provider's signature for the Token Status List that keeps the status of a WUA
+         *
+         * Can be used by a Credential Issuer, issuing device-bound attestations to keep track of WUA status
+         */
         EU_WUA_STATUS,
+
+        /**
+         * Check PID Provider's signature for a PID
+         *
+         * Can be used by Wallets after issuance and Verifiers during presentation verification
+         */
         EU_PID,
+
+        /**
+         * Check the signature of a Status Lists or Identifiers List that keeps the status of a PID
+         *
+         * Can be used by Wallets and Verifiers to check the status of a PID
+         */
         EU_PID_STATUS,
+
+        /**
+         * Check the issuer's signature for a Public EAA
+         *
+         * Can be used by Wallets after issuance and Verifiers during presentation verification
+         */
         EU_PUB_EAA,
+
+        /**
+         * Check the signature of a Status Lists or Identifiers List that keeps the status of a Public EAA
+         *
+         * Can be used by Wallets and Verifiers to check the status of a PUB_EAA
+         */
         EU_PUB_EAA_STATUS,
+
+        /**
+         * Check the signature of a registration certificate of an Issuer or Verifier
+         *
+         * Can be used by Wallets to verify the signature of the registration certificate of an Issuer or Verifier, during
+         * issuance and presentation respectively.
+         */
         EU_WRPRC,
         EU_WRPRC_STATUS,
+
+        /**
+         * Check the access certificate of an Issuer or Verifier
+         *
+         * Can be used by Wallets to verify the signature of the registration certificate of an Issuer or Verifier, during
+         * issuance (signed credential issuer metadata) and presentation respectively (signed authorization request).
+         */
         EU_WRPAC,
         EU_WRPAC_STATUS,
+
+        /**
+         * Check mDL Provider's signature for an mDL
+         * Can be used by Wallets and Verifiers to check the status of an mDL
+         */
         EU_MDL,
+
+        /**
+         * Check the signature of a Status Lists or Identifiers List that keeps the status of an mDL
+         */
         EU_MDL_STATUS,
     }
 
