6265dda4f3ccd9a2426758c2f42935cdc7e1c484
Anastasios Tsantoulis <anastasios.tsantoulis@netcompany.com>
2026-02-18 15:30:04 +0200

    Use `depedencyCheckAggregate` instead of `depedencyCheckAnalyze` for multi-module project (#48)

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,13 @@ plugins {
     alias(libs.plugins.spotless) apply false
     alias(libs.plugins.dokka) apply false
     alias(libs.plugins.maven.publish) apply false
-    alias(libs.plugins.dependency.check) apply false
     alias(libs.plugins.atomicfu) apply false
+    alias(libs.plugins.dependency.check) apply true
+}
+
+dependencyCheck {
+    formats = listOf("XML", "HTML")
+    nvd.apiKey = System.getenv("NVD_API_KEY") ?: properties["nvdApiKey"]?.toString() ?: ""
+    nvd.delay = 10000
+    nvd.maxRetryCount = 2
 }
