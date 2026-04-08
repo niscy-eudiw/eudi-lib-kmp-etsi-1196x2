08ba18eb283f27c97d85ccb4650f2a4c56e88833
babisRoutis <haralampos.routis@netcompany.com>
2026-04-08 11:51:55 +0300

    Add iOS targets to Gradle build scripts
    Signed-off-by: babisRoutis <haralampos.routis@netcompany.com>

--- consultation/build.gradle.kts
+++ consultation/build.gradle.kts
@@ -56,6 +56,11 @@ kotlin {
             }
     }
 
+    // iOS targets
+    iosArm64()
+    iosX64()
+    iosSimulatorArm64()
+
     // Set up targets
     @OptIn(ExperimentalKotlinGradlePluginApi::class)
     applyDefaultHierarchyTemplate {
